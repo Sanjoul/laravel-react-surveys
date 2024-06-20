209ecf46d4ded684d1b19722b6c02473ddda97cb
Sanjoul Bhusal <bsanjoul@gmail.com>
2024-06-20 08:36:02 +0545

    Laravel-react project

---
+++ react/src/router.jsx
@@ -0,0 +1,46 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+
+import Dashboard from "./views/Dashboard";
+import Login from "./views/Login";
+import Signup from "./views/Signup";
+import Surveys from "./views/Surveys";
+import GuestLayyout from "./components/GuestLayyout";
+import DefaultLayout from "./components/DefaultLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: "/dashboard",
+        element: <Navigate to="/" />,
+      },
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+      {
+        path: "/surveys",
+        element: <Surveys />,
+      },
+    ],
+  },
+
+  {
+    path: "/",
+    element: <GuestLayyout />,
+    children: [
+      {
+        path: "login",
+        element: <Login />,
+      },
+
+      {
+        path: "signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+export default router;
